03de9550050dcb8e46d525b5a79edb6738e682d5
MrAleos <jin.tekkenmobile.ru@gmail.com>
2024-12-28 13:44:25 +0300

    Изменил gendiff.js для вывода информации по флагу -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,5 +3,6 @@ import { program } from 'commander';
 program
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, format [type]', 'output format')
     .helpOption('-h, --help', 'output usage information');
 program.parse();
