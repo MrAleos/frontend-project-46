e86fd00f9b8dd5b0dfe3eec161161b958a36737b
MrAleos <jin.tekkenmobile.ru@gmail.com>
2024-12-28 13:34:10 +0300

    Добавил package.json, .gitignore и gendiff.js с выводом справки

---
+++ bin/gendiff.js
@@ -0,0 +1,7 @@
+import { program } from 'commander';
+
+program
+    .version('1.0.0')
+    .description('Compares two configuration files and shows a difference.')
+    .helpOption('-h, --help', 'output usage information');
+program.parse();
